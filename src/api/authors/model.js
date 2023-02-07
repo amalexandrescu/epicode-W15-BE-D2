e0ec0f1e044c3d3bd2690be93756d403c7d3b043
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-02-07 14:56:52 +0200

    adds basicAuth  and admin only authorization  and checkCredentials method to authors schema

--- src/api/authors/model.js
+++ src/api/authors/model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema, model } = mongoose;
 
@@ -8,10 +9,26 @@ const authorsSchema = new Schema(
     surname: { type: String, required: true },
     email: { type: String, required: true },
     birthDate: { type: Date, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
   },
   {
     timestamps: true,
   }
 );
+
+authorsSchema.static("checkCredentials", async function (email, password) {
+  const author = await this.findOne({ email });
+  if (author) {
+    const passwordMatch = await bcrypt.compare(password, author.password);
+    if (passwordMatch) {
+      return author;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
 
 export default model("Author", authorsSchema);
---
+++ src/library/authentication/basicAuth.js
@@ -0,0 +1,24 @@
+import createHttpError from "http-errors";
+import atob from "atob";
+import AuthorsModel from "../../api/authors/model.js";
+
+export const basicAuthMiddleware = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    next(
+      createHttpError(
+        401,
+        "Please provide credentials in the Authorization header!"
+      )
+    );
+  } else {
+    const encodedCredentials = req.headers.authorization.split(" ")[1];
+    const decodedCredentials = atob(encodedCredentials);
+    const [email, password] = decodedCredentials.split(":");
+    const author = await AuthorsModel.checkCredentials(email, password);
+    if (author) {
+      req.author = author;
+    } else {
+      next(createHttpError(401, "Credentials not ok!"));
+    }
+  }
+};
